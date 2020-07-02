74f9302c7d382218e03ccf6c1b5e5102382d00fc
Elias Khan <khan.elias@gmail.com>
2020-07-02 15:33:37 +0200

    Whale

--- src/main/java/be/vdab/model/Fish.java
+++ src/main/java/be/vdab/model/Fish.java
@@ -1,4 +1,16 @@
 package be.vdab.model;
 
-public abstract class Fish {
+public abstract class Fish extends Animal {
+
+    private boolean hasTeeth;
+
+    public abstract void talk();
+
+    public boolean isHasTeeth() {
+        return hasTeeth;
+    }
+
+    public void setHasTeeth(boolean hasTeeth) {
+        this.hasTeeth = hasTeeth;
+    }
 }
--- src/main/java/be/vdab/model/mamals/Whale.java
+++ src/main/java/be/vdab/model/mamals/Whale.java
@@ -1,6 +1,15 @@
 package be.vdab.model.mamals;
 
+import be.vdab.model.Breathable;
 import be.vdab.model.Mamal;
 
-public class Whale extends Mamal {
+public class Whale extends Mamal implements Breathable {
+
+    public void eat() {
+        System.out.println("nomming on some plankton");
+    }
+
+    public void move() {
+        System.out.println("moves reallllly whaley");
+    }
 }
