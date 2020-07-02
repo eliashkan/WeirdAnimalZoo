b3e6c644379eb93005d6dd7108d50d7b644a5919
Elias Khan <khan.elias@gmail.com>
2020-07-02 15:28:11 +0200

    Animal class

--- src/main/java/be/vdab/model/Animal.java
+++ src/main/java/be/vdab/model/Animal.java
@@ -1,4 +1,36 @@
 package be.vdab.model;
 
 public abstract class Animal {
+
+    private String name;
+    private double weight;
+    private int age;
+
+    public abstract void eat();
+
+    public abstract void move();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
