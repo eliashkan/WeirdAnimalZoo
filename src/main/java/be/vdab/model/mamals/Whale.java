4f7a8b52d32d27a7bf273fa7d2056dad43b024d7
Elias Khan <khan.elias@gmail.com>
2020-07-02 15:36:10 +0200

    Whale done

--- src/main/java/be/vdab/model/mamals/Whale.java
+++ src/main/java/be/vdab/model/mamals/Whale.java
@@ -2,8 +2,11 @@ package be.vdab.model.mamals;
 
 import be.vdab.model.Breathable;
 import be.vdab.model.Mamal;
+import be.vdab.model.fish.ParasiteFish;
 
 public class Whale extends Mamal implements Breathable {
+
+    ParasiteFish[] parasites = new ParasiteFish[0];
 
     public void eat() {
         System.out.println("nomming on some plankton");
@@ -11,5 +14,9 @@ public class Whale extends Mamal implements Breathable {
 
     public void move() {
         System.out.println("moves reallllly whaley");
+    }
+
+    public void breathe() {
+        System.out.println("breathes out through a hole");
     }
 }
