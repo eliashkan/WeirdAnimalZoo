6455d1fd8307af37f8ba532e04f03f0b4ae59967
Elias Khan <khan.elias@gmail.com>
2020-07-02 15:30:32 +0200

    Mamal getters and setters

--- src/main/java/be/vdab/model/Mamal.java
+++ src/main/java/be/vdab/model/Mamal.java
@@ -5,4 +5,19 @@ public abstract class Mamal extends Animal implements Breathable {
     private int legCount;
     private boolean canSwim;
 
+    public int getLegCount() {
+        return legCount;
+    }
+
+    public void setLegCount(int legCount) {
+        this.legCount = legCount;
+    }
+
+    public boolean isCanSwim() {
+        return canSwim;
+    }
+
+    public void setCanSwim(boolean canSwim) {
+        this.canSwim = canSwim;
+    }
 }
